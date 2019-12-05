627050c3a7951108f4a4811f3802c6840215c993
siemens-c2c <joy_mon78@yahoo.co.in>
2019-12-05 14:28:56 +0530

    created payroll management system application
    this application calculates salary of an employee and prints the same

---
+++ Codes/Day-2/PMSAPP/EmployeeLibrary/Employee.cs
@@ -0,0 +1,49 @@
+﻿namespace EmployeeLibrary
+{
+    /// <summary>
+    /// class representing an employee
+    /// </summary>
+    public class Employee
+    {
+        private int employeeId;
+        private string employeeName;
+        private decimal basicPayment;
+        private decimal daPayment;
+        private decimal hraPayment;
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Employee()
+        {
+
+        }
+        /// <summary>
+        /// parameterized constructor
+        /// </summary>
+        /// <param name="id">id of employee</param>
+        /// <param name="name">name of employee</param>
+        /// <param name="basicPay">basic payment of employee</param>
+        /// <param name="daPay">da payment of employee</param>
+        /// <param name="hraPay">hra payment of employee</param>
+        public Employee(int id, string name, decimal basicPay, decimal daPay, decimal hraPay)
+        {
+            employeeId = id;
+            employeeName = name;
+            basicPayment = basicPay;
+            daPayment = daPay;
+            hraPayment = hraPay;
+        }
+        /// <summary>
+        /// method to calculate total salary of an employee
+        /// </summary>
+        /// <returns>
+        /// returns calculated total salary of an employee
+        /// </returns>
+        public decimal CalculateSalary()
+        {
+            decimal totalSalary = basicPayment + daPayment + hraPayment;
+            return totalSalary;
+        }
+    }
+}
---
+++ Codes/Day-2/PMSAPP/PayRollClinet/Program.cs
@@ -0,0 +1,44 @@
+﻿using EmployeeLibrary;
+using System;
+
+namespace PayRollClient
+{
+    class Program
+    {
+        static Employee CreateEmployee()
+        {
+            //1. Accepting values from user
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Basic Pay: ");
+            decimal basicPay = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Da Pay: ");
+            decimal daPay = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Hra Pay: ");
+            decimal hraPay = decimal.Parse(Console.ReadLine());
+
+            //2. creating an instance of employee class by invoking parameterized constructor and passing the values to it
+            Employee employeeReference = new Employee(id, name, basicPay, daPay, hraPay);
+
+            //3. returning the reference to the Main method
+            return employeeReference;
+        }
+        static void Main()
+        {
+            //calling CreateEmployee method to create an instance of an employee class
+            Employee employee = CreateEmployee();
+
+            //calling CalculateSalary method to calculate salary
+            decimal totalSalary = employee.CalculateSalary();
+
+            //printing total salary
+            Console.WriteLine($"Total Salary: {totalSalary}");
+        }
+    }
+}
