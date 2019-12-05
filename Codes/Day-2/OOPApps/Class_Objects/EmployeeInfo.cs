fdb69a789cfcfda970b78a2954a065362dfc4ad2
siemens-c2c <joy_mon78@yahoo.co.in>
2019-12-05 12:51:40 +0530

    created OOP code
    OOP code explaining class, object, ctor and static, const etc.

---
+++ Codes/Day-2/OOPApps/Class_Objects/EmployeeInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Objects
+{
+    class EmployeeInfo
+    {
+        private int empId;
+        private string empName;
+        private string empDept;
+        private decimal empSalary;
+        //public const string company = "siemens";
+        //public static string company = "siemens";
+        static string company;
+
+        static EmployeeInfo()
+        {
+            company = "siemens";
+        }
+
+        //default constructor
+        public EmployeeInfo()
+        {
+            //empDept = null;
+            //empName = null;
+            //empId = 0;
+            //empSalary = 0.0M;
+        }
+        public EmployeeInfo(int id, string name, string dept, decimal salary)
+        {
+            empId = id;
+            empName = name;
+            empDept = dept;
+            empSalary = salary;
+        }
+        //public void Assign(int id, string name, string dept, decimal salary)
+        //{
+        //    empId = id;
+        //    empName = name;
+        //    empDept = dept;
+        //    empSalary = salary;
+        //    company = "";
+        //}
+        //public static void Test()
+        //{
+
+        //}
+        public string Print()
+        {
+            return $"Name: {empName}, Id: {empId}, Salary:{empSalary} and department: {empDept} belongs to {company}";
+        }
+    }
+}
---
+++ Codes/Day-2/OOPApps/Class_Objects/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Objects
+{
+    class Program
+    {
+        static EmployeeInfo GetValues()
+        {
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Id: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.Write("Salary: ");
+            decimal salary = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Department: ");
+            string department = Console.ReadLine();
+
+            //EmployeeInfo empReference = new EmployeeInfo();
+            EmployeeInfo empReference = new EmployeeInfo(id, name, department, salary);
+            //empReference.Assign(id, name, department, salary);
+            return empReference;
+        }
+        static void Main()
+        {
+            //for (int i = 0; i < 20; i++)
+            //{
+            EmployeeInfo infoAddress = GetValues();
+            string info = infoAddress.Print();
+            Console.WriteLine(info);
+            //}
+        }
+    }
+}
